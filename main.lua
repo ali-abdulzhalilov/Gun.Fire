43c0d70293db02a07fcfd359a7c3d10787930f24
mark-solo <clueless.magician@gmail.com>
2019-01-17 20:11:34 +0300

    basic setup

---
+++ main.lua
@@ -0,0 +1,14 @@
+Object = require "lib.classic"
+local bump = require "lib.bump"
+
+function love.load()
+  
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+  love.graphics.print("Hello, World!", 100, 100)
+end
