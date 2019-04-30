91f01a5db2504611379705772e17092cf2ac49f4
mark-solo <mark.an.impostor@gmail.com>
2019-04-30 14:04:57 +0300

    bullet disabled on touch

--- object/entity/bullet.lua
+++ object/entity/bullet.lua
@@ -1,33 +1,44 @@
 Bullet = Entity:extend()
 
 function Bullet:new(scene, world, x, y)
-  Bullet.super.new(self, scene, world, -100, -100, 0.5, 0.5)
+  Bullet.super.new(self, scene, world, 0, 0, 0.5, 0.5)
+  self:setAlive(false)
 end
 
 function Bullet:boop(x, y, dx, dy)
   self:setAlive(true)
   self.x, self.y = x, y
-  --self.world:update(self, x, y)
+  self.world:update(self, x, y)
   self:move(dx, dy)
 end
 
 function Bullet:setAlive(value)
   if value then
-    if not self.world:hasItem(self) then
-      self.world:add(self.x, self.y, self.w, self.h)
+    if not self:isAlive() then
+      self.world:add(self, self.x, self.y, self.w, self.h)
     end
   else
-    if self.world:hasItem(self) then
+    if self:isAlive() then
       self.world:remove(self)
     end
   end
 end
 
---function Bullet:update(dt)
---  if self:isAlive() then
---    Bullet.super.update(self, dt)
---  end
---end
+function Bullet:isAlive()
+  return self.world:hasItem(self)
+end 
+
+function Bullet:update(dt)
+  if self:isAlive() then
+    Bullet.super.update(self, dt)
+  end
+end
+
+function Bullet:draw()
+  if self:isAlive() then
+    Bullet.super.draw(self, dt)
+  end
+end
 
 function Bullet:filter(item, other)
   --if type(item)=="number" then
@@ -37,8 +48,10 @@ function Bullet:filter(item, other)
   --end
 end
 
---function Bullet:resolve(cols, len)
-  --if self:isAlive() then 
-  --  self:setAlive(false)
-  --end
---end
+function Bullet:resolve(cols, len)
+  for i=1,len do
+    if type(cols[i].other)=="number" then
+      self:setAlive(false)
+    end
+  end
+end
